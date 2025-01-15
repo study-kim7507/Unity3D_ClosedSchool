684b23c1e4e342045860f00f096fa9757f480b59
GreenPlum0908 <kijoo20000908@gmail.com>
2025-01-16 00:25:44 +0900

    test

---
+++ Assets/WorkSpace/kijoo20000908/02.Scripts/Puzzle1.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Puzzle1 : MonoBehaviour, IInteractable
+{
+    public void BeginFocus()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void BeginInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EndFocus()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EndInteract()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Interact()
+    {
+
+    }
+}
